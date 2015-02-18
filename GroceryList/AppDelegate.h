2447a4090709d10b657902c5932f39ea6d122ec6
damonskinner <dcs250@gmail.com>
2015-02-18 11:39:41 -0500

    Initial Commit

---
+++ GroceryList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GroceryList
+//
+//  Created by Damon Skinner on 2/18/15.
+//  Copyright (c) 2015 Damon Skinner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GroceryList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GroceryList
+//
+//  Created by Damon Skinner on 2/18/15.
+//  Copyright (c) 2015 Damon Skinner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
